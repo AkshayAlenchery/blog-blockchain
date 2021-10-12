6268c9cb7ad55dabcd596e050e2500ec2ee26f1f
Akshay <akshayalenchery@gmail.com>
2021-10-12 11:48:47 +0530

    Init commit

---
+++ x/blog/handler.go
@@ -0,0 +1,29 @@
+package blog
+
+import (
+	"fmt"
+
+	"github.com/akshayalenchery/blog/x/blog/keeper"
+	"github.com/akshayalenchery/blog/x/blog/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// NewHandler ...
+func NewHandler(k keeper.Keeper) sdk.Handler {
+	// this line is used by starport scaffolding # handler/msgServer
+
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+
+		switch msg := msg.(type) {
+		// this line is used by starport scaffolding # 1
+		case *types.MsgCreatePost:
+			return handleMsgCreatePost(ctx, k, msg)
+			
+		default:
+			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
+			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
+		}
+	}
+}
